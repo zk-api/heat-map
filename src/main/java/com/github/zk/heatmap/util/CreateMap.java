b2d6814751de9bd87132cfd51d726ec38e683010
e <506683059@qq.com>
2020-05-20 14:37:43 +0800

    新增数值范围和颜色范围接口

--- src/main/java/com/github/zk/heatmap/util/CreateMap.java
+++ src/main/java/com/github/zk/heatmap/util/CreateMap.java
@@ -6,7 +6,6 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -16,17 +15,11 @@ import java.util.List;
 
 /**
  * @author zk
+ * @version v1.0.0
  * @date 2019/8/2 9:28
  * @since v1.0.0
- * @version v1.0.0
  */
 public class CreateMap {
-
-    /**
-     * 初始化数值范围和颜色值对应关系
-     */
-    private static List<int[]> valueList = initValue();
-    private static List<Color> colorList = initColor();
 
     /**
      * 使用读取文件方式示例
@@ -36,7 +29,7 @@ public class CreateMap {
      */
     public static void main(String[] args) {
         List<HeatMapEntity> list = new ArrayList<>();
-        Path path = Paths.get("E:\\githubWork\\heat-map\\data\\dop.DOP");
+        Path path = Paths.get("D:\\work\\workspace\\github\\heat-map\\data\\dop.DOP");
         try {
             Files.lines(path).filter(line -> {
                 boolean flag = false;
@@ -47,41 +40,35 @@ public class CreateMap {
             }).forEach(line -> {
                 HeatMapEntity hme = new HeatMapEntity();
                 String[] lines = line.split("\\s+");
-                if (Double.valueOf(lines[1]) % 1 == 0 && Double.valueOf(lines[2]) % 1 == 0) {
-                    hme.setLat(Double.valueOf(lines[1]));
-                    hme.setLon(Double.valueOf(lines[2]));
-                    hme.setValue(lines[4].equals("--") ? 999999d : Double.valueOf(lines[4]));
+                if (Double.parseDouble(lines[1]) % 1 == 0 && Double.parseDouble(lines[2]) % 1 == 0) {
+                    hme.setLat(Double.parseDouble(lines[1]));
+                    hme.setLon(Double.parseDouble(lines[2]));
+                    hme.setValue("--".equals(lines[4]) ? 999999d : Double.parseDouble(lines[4]));
                     list.add(hme);
                 }
             });
         } catch (IOException e) {
             e.printStackTrace();
         }
-        boolean b = creatHeatMap(list, "E:\\githubWork\\heat-map\\data\\dt-release.png", "E:\\githubWork\\heat-map\\data\\outpic\\2.png", -150, 90);
+        boolean b = creatHeatMap(list, "D:\\work\\workspace\\github\\heat-map\\data\\dt-release.png",
+                "D:\\work\\workspace\\github\\heat-map\\data\\outpic\\3.png", defaultValue(), defaultColor(), 0, 90);
         if (b) {
             System.out.println("成功");
         }
     }
 
-    public static boolean creatHeatMap(List<HeatMapEntity> list, String outPath) {
-        return creatHeatMap(list, null, outPath, -180, 90);
-    }
-
     /**
-     * 生成热力图
-     *
-     * @param list           元数据
-     * @param backgroundPath 背景图
-     * @param outPath        输出路径
+     * 生成默认位置默认颜色热力图
+     * @param list    元数据
+     * @param outPath 输出路径
      * @return
-     * @since v1.0.0
      */
-    public static boolean creatHeatMap(List<HeatMapEntity> list, String backgroundPath, String outPath) {
-        return creatHeatMap(list, backgroundPath, outPath, -180, 90);
+    public static boolean creatHeatMap(List<HeatMapEntity> list, String outPath) {
+        return creatHeatMap(list, null, outPath, null, null, -180, 90);
     }
 
     /**
-     * 根据经纬度位置生成热力图
+     * 生成自定义位置和自定义颜色带背景的热力图
      *
      * @param list           元数据
      * @param backgroundPath 背景图
@@ -91,16 +78,17 @@ public class CreateMap {
      * @return
      * @since v1.0.0
      */
-    public static boolean creatHeatMap(List<HeatMapEntity> list, String backgroundPath, String outPath, int startLon, int startLat) {
+    public static boolean creatHeatMap(List<HeatMapEntity> list, String backgroundPath, String outPath, List<int[]> values, List<Color> colors, int startLon, int startLat) {
         //初始化图片缓冲区(width:3900 height:1970)
         BufferedImage bi = new BufferedImage(3900, 1970, BufferedImage.TYPE_INT_ARGB);
         //初始化画板
         Graphics2D graphics = bi.createGraphics();
 
         //初始化数值范围
-        List<int[]> initValue = initValue();
+        List<int[]> initValue = (values == null ? defaultValue() : values);
+
         //初始化颜色范围
-        List<Color> initColor = initColor();
+        List<Color> initColor = (colors == null ? defaultColor() : colors);
         //为每一个点查找颜色值
         for (HeatMapEntity entity : list) {
             for (int i = 0; i < initValue.size(); i++) {
@@ -153,10 +141,11 @@ public class CreateMap {
 
     /**
      * 初始化数值范围
+     *
      * @return
-     * @since v1.0.0
+     * @since v2.0.0
      */
-    private static List<int[]> initValue() {
+    private static List<int[]> defaultValue() {
         List<int[]> list = new ArrayList<>(9);
         list.add(new int[]{0, 1});
         list.add(new int[]{1, 2});
@@ -172,10 +161,11 @@ public class CreateMap {
 
     /**
      * 初始化颜色范围
+     *
      * @return
-     * @since v1.0.0
+     * @since v2.0.0
      */
-    private static List<Color> initColor() {
+    private static List<Color> defaultColor() {
         List<Color> list = new ArrayList<>(9);
         list.add(new Color(0, 0, 220, 255));
         list.add(new Color(40, 40, 255, 255));
