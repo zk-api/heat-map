331b7dff73ca517166b52b173ffb706d87b46ac1
zhaokai <506683059@qq.com>
2022-07-28 15:34:27 +0800

    调整热力图功能

--- src/main/java/com/github/zk/heatmap/util/CreateMap.java
+++ src/main/java/com/github/zk/heatmap/util/CreateMap.java
@@ -12,7 +12,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author zk
@@ -29,8 +32,54 @@ public class CreateMap {
      * @since v1.0.0
      */
     public static void main(String[] args) {
+//        dop();
+        dop1();
+    }
+
+    public static void dop() {
         List<HeatMapEntity> list = new ArrayList<>();
-        Path path = Paths.get("C:\\Users\\zhaokai\\Desktop\\global_2020-01-01.avl");
+        Path path = Paths.get("C:\\Users\\zhaokai\\Desktop\\rr_bds_pos_178_09.22r");
+        try {
+            Files.lines(path).filter(line -> {
+                boolean flag = false;
+                if (!line.startsWith("#")) {
+                    flag = true;
+                }
+                return flag;
+            }).forEach(line -> {
+                HeatMapEntity hme = new HeatMapEntity();
+                String[] lines = line.split("\\s+");
+                hme.setLat(Double.parseDouble(lines[3]));
+                hme.setLon(Double.parseDouble(lines[2]));
+                hme.setValue("--".equals(lines[6]) ? 999999d : Double.parseDouble(lines[6]));
+                list.add(hme);
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        List<double[]> list0 = new ArrayList<>(9);
+        list0.add(new double[]{0d, 2d});
+        list0.add(new double[]{2d, 4d});
+        list0.add(new double[]{4d, 6d});
+        list0.add(new double[]{6d, 8d});
+        list0.add(new double[]{8d, 10d});
+
+        List<Color> list1 = new ArrayList<>();
+        list1.add(new Color(0, 0, 207, 255));
+        list1.add(new Color(0, 223, 255, 255));
+        list1.add(new Color(0, 223, 255, 255));
+        list1.add(new Color(255, 207, 0, 255));
+        list1.add(new Color(255, 143, 0, 255));
+        boolean b = createHeatMapByBackground(list, "D:\\work\\workspace\\github\\heat-map\\data\\dt-release.png", "C:\\Users\\zhaokai\\Desktop\\rr_bds_pos_178_09.png",
+                4.0d, 2.0d, list0, list1, -180, 90);
+        if (b) {
+            System.out.println("成功");
+        }
+    }
+
+    public static void dop1() {
+        List<HeatMapEntity> list = new ArrayList<>();
+        Path path = Paths.get("D:\\work\\workspace\\github\\heat-map\\data\\dop.DOP");
         try {
             Files.lines(path).filter(line -> {
                 boolean flag = false;
@@ -47,19 +96,91 @@ public class CreateMap {
 //                    hme.setValue("--".equals(lines[3]) ? 999999d : Double.parseDouble(lines[3]));
 //                    list.add(hme);
 //                }
-                hme.setLat(Double.parseDouble(lines[0]));
-                hme.setLon(Double.parseDouble(lines[1]));
+                hme.setLat(Double.parseDouble(lines[1]));
+                hme.setLon(Double.parseDouble(lines[2]));
                 hme.setValue("--".equals(lines[4]) ? 999999d : Double.parseDouble(lines[4]));
                 list.add(hme);
             });
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-//        boolean b = creatHeatMap(list, "D:\\work\\workspace\\github\\heat-map\\data\\dt-release.png",
-//                "D:\\work\\workspace\\github\\heat-map\\data\\outpic\\3.png", defaultValue(), defaultColor(), 0, 90);
-        boolean b = creatHeatMap(list, "C:\\Users\\zhaokai\\Desktop\\2.png", 5.0, 2.5);
+
+        List<double[]> list0 = new ArrayList<>(9);
+        list0.add(new double[]{0d, 1d});
+        list0.add(new double[]{1d, 2d});
+        list0.add(new double[]{2d, 3d});
+        list0.add(new double[]{3d, 4d});
+        list0.add(new double[]{4d, 5d});
+        list0.add(new double[]{5d, 6d});
+        list0.add(new double[]{6d, 7d});
+        list0.add(new double[]{7d, 8d});
+        list0.add(new double[]{8d, 9d});
+
+        List<Color> list1 = new ArrayList<>();
+        list1.add(new Color(134, 153, 9, 255));
+        list1.add(new Color(134, 153, 9, 255));
+        list1.add(new Color(44, 207, 224, 255));
+        list1.add(new Color(189, 9, 207, 255));
+        list1.add(new Color(255, 255, 0, 255));
+        list1.add(new Color(4, 3, 193, 255));
+        list1.add(new Color(224, 16, 37, 255));
+        list1.add(new Color(4, 99, 36, 255));
+        list1.add(new Color(255, 255, 255, 255));
+        boolean b = createHeatMapByBackground(list, "D:\\work\\workspace\\github\\heat-map\\data\\dt-release.png", "C:\\Users\\zhaokai\\Desktop\\0.png",
+                 list0, list1, -180, 90);
         if (b) {
             System.out.println("成功");
+        }
+    }
+
+    public static void codg() {
+        List<HeatMapEntity> list = new ArrayList<>();
+        Path path = Paths.get("C:\\Users\\zhaokai\\Desktop\\t.txt");
+        try {
+            Iterator<String> iterator = Files.lines(path).iterator();
+            while(iterator.hasNext()) {
+                String firstLine = iterator.next();
+                //纬度
+                String lat = firstLine.substring(0, 8).trim();
+                BigDecimal latBigDecimal = new BigDecimal(lat);
+                //开始经度
+                String startLon = firstLine.substring(8, 12);
+                BigDecimal startLonBigDecimal = new BigDecimal(startLon);
+                //结束经度
+                String endLon = firstLine.substring(15, 20);
+                BigDecimal endLonBigDecimal = new BigDecimal(endLon);
+                //步长
+                String step = firstLine.substring(23, 26);
+                BigDecimal stepBigDecimal = new BigDecimal(step);
+
+                List<String> value = new ArrayList<>();
+                Collections.addAll(value, iterator.next().split("\\s+"));
+                Collections.addAll(value, iterator.next().split("\\s+"));
+                Collections.addAll(value, iterator.next().split("\\s+"));
+                Collections.addAll(value, iterator.next().split("\\s+"));
+                Collections.addAll(value, iterator.next().split("\\s+"));
+                List<String> collect = value.stream().filter(s -> !s.isEmpty()).collect(Collectors.toList());
+                //等差数列求项数
+                BigDecimal forLength = new BigDecimal("1")
+                        .add(endLonBigDecimal
+                                .subtract(startLonBigDecimal)
+                                .divide(stepBigDecimal, BigDecimal.ROUND_UP));
+                for (int i = 0; i < forLength.intValue(); i++) {
+                    HeatMapEntity heatMapEntity = new HeatMapEntity();
+                    heatMapEntity.setLat(latBigDecimal.doubleValue());
+                    heatMapEntity.setLon(startLonBigDecimal.doubleValue());
+                    heatMapEntity.setValue(Double.parseDouble(collect.get(i)));
+                    startLonBigDecimal = startLonBigDecimal.add(stepBigDecimal);
+                    list.add(heatMapEntity);
+                }
+            }
+            boolean b = createHeatMap(list, "C:\\Users\\zhaokai\\Desktop\\1.png", 5.0, 2.5);
+            if (b) {
+                System.out.println("成功");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -70,8 +191,8 @@ public class CreateMap {
      * @param outPath 输出路径
      * @return
      */
-    public static boolean creatHeatMap(List<HeatMapEntity> list, String outPath) {
-        return creatHeatMap(list, null, outPath, null, null,
+    public static boolean createHeatMap(List<HeatMapEntity> list, String outPath) {
+        return createHeatMapByBackground(list, null, outPath, null, null,
                 -180, 90);
     }
 
@@ -90,7 +211,7 @@ public class CreateMap {
     }
 
     /**
-     * 使用默认范围和默认颜色生成带背景地图的热力图
+     * 使用默认颜色生成带背景地图的热力图
      *
      * @param list           元数据
      * @param backgroundPath 背景地图路径
@@ -106,6 +227,8 @@ public class CreateMap {
     }
 
     /**
+     * 使用默认1x1像素生成带背景的热力图
+     *
      * @param list           元数据
      * @param backgroundPath 背景地图路径
      * @param outPath        输出路径
@@ -156,7 +279,7 @@ public class CreateMap {
                     if (lon < 0) {
                         lon += 360;
                     }
-                    graphics.fillRect((lon * 10) + 145, (lat * 10) + 80, 50, 50);
+                    graphics.fillRect((lon * 10) + 145, (lat * 10) + 80, 10, 10);
                     break;
                 }
             }
@@ -183,23 +306,6 @@ public class CreateMap {
     }
 
     /**
-     * 生成默认位置默认颜色带背景的热力图
-     *
-     * @param list
-     * @param backgroundPath
-     * @param outPath
-     * @return
-     */
-    public static boolean creatHeatMap(List<HeatMapEntity> list, String backgroundPath, String outPath) {
-        if (backgroundPath != null) {
-            return creatHeatMap(list, backgroundPath, outPath, null, null,
-                    -180, 90);
-        } else {
-            return creatHeatMap(list, outPath);
-        }
-    }
-
-    /**
      * 生成默认色值，无背景，可变像素热力图
      *
      * @param list    数据
@@ -208,7 +314,7 @@ public class CreateMap {
      * @param height  纬度步长
      * @return
      */
-    public static boolean creatHeatMap(List<HeatMapEntity> list, String outPath, Double width, Double height) {
+    public static boolean createHeatMap(List<HeatMapEntity> list, String outPath, Double width, Double height) {
         //初始化图片缓冲区
         BufferedImage bi = new BufferedImage(3600, 1800, BufferedImage.TYPE_INT_ARGB);
         //初始化画板
@@ -274,8 +380,8 @@ public class CreateMap {
      * @param values  数值范围
      * @return
      */
-    public static boolean creatHeatMap(List<HeatMapEntity> list, String outPath, Double width, Double height,
-                                       List<double[]> values, List<Color> colors) {
+    public static boolean createHeatMap(List<HeatMapEntity> list, String outPath, Double width, Double height,
+                                        List<double[]> values, List<Color> colors) {
         //初始化图片缓冲区
         BufferedImage bi = new BufferedImage(3600, 1800, BufferedImage.TYPE_INT_ARGB);
         //初始化画板
@@ -342,7 +448,8 @@ public class CreateMap {
      * @return
      * @since v1.0.0
      */
-    public static boolean creatHeatMap(List<HeatMapEntity> list, String backgroundPath, String outPath,
+    public static boolean createHeatMapByBackground(List<HeatMapEntity> list, String backgroundPath, String outPath,
+                                       Double width, Double height,
                                        List<double[]> values, List<Color> colors, int startLon, int startLat) {
         //初始化图片缓冲区(width:3900 height:1970)
         BufferedImage bi = new BufferedImage(3900, 1970, BufferedImage.TYPE_INT_ARGB);
@@ -382,7 +489,8 @@ public class CreateMap {
                     if (lon < 0) {
                         lon += 360;
                     }
-                    graphics.fillRect((lon * 10) + 145, (lat * 10) + 80, 50, 50);
+                    graphics.fillRect((lon * 10) + 145, (lat * 10) + 80,
+                            (int) (10 * width), (int) (10 * height));
                     break;
                 }
             }
@@ -392,6 +500,79 @@ public class CreateMap {
             ImageIcon ii = new ImageIcon(backgroundPath);
             //绘制地图
             graphics.drawImage(ii.getImage(), 0, 0, 3900, 1970, null);
+        }
+
+        try {
+            Path path = Paths.get(outPath);
+            if (!Files.exists(path)) {
+                Files.createDirectories(path.getParent());
+            }
+            ImageIO.write(bi, "png", path.toFile());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        bi.flush();
+        return true;
+    }
+
+    /**
+     * 生成无背景，自定义像素、数值和色值、范围热力图
+     * @param list 数据集合
+     * @param outPath 输出图片地址
+     * @param width 像素宽
+     * @param height 像素高
+     * @param values 数值
+     * @param colors 色值
+     * @param startLon 图片开始精度
+     * @param startLat 图片开始纬度
+     * @return
+     */
+    public static boolean createHeatMap(List<HeatMapEntity> list, String outPath, Double width, Double height,
+                                       List<double[]> values, List<Color> colors,  int startLon, int startLat) {
+        //初始化图片缓冲区
+        BufferedImage bi = new BufferedImage(3600, 1800, BufferedImage.TYPE_INT_ARGB);
+        //初始化画板
+        Graphics2D graphics = bi.createGraphics();
+
+        //初始化数值范围
+        List<double[]> initValue = (values == null ? defaultValue() : values);
+
+        //初始化颜色范围
+        List<Color> initColor = (colors == null ? defaultColor() : colors);
+        //为每一个点查找颜色值
+        for (HeatMapEntity entity : list) {
+            for (int i = 0; i < initValue.size(); i++) {
+                double start = initValue.get(i)[0];
+                double end = initValue.get(i)[1];
+                int startBigDecimal = BigDecimal.valueOf(start).intValue();
+                int endBigDecimal = BigDecimal.valueOf(end).intValue();
+                int valueBigDecimal = BigDecimal.valueOf(entity.getValue()).intValue();
+                if (valueBigDecimal - startBigDecimal >= 0 &&
+                        valueBigDecimal - endBigDecimal < 0) {
+                    graphics.setColor(initColor.get(i));
+                    int lat = (int) entity.getLat();
+                    int lon = (int) entity.getLon();
+                    //纬度90转换为0
+                    lat -= 90;
+                    lat = Math.abs(lat);
+                    //偏移startLat后，纬度坐标
+                    lat -= 90 - startLat;
+                    if (lat < 0) {
+                        lat += 180;
+                    }
+                    //经度-180转换为0·
+                    lon += 180;
+                    //偏移startLon后，经度坐标
+                    lon += (-180 - startLon);
+                    if (lon < 0) {
+                        lon += 360;
+                    }
+                    graphics.fillRect((int) (lon * 10), (int) (lat * 10),
+                            (int) (10 * width), (int) (10 * height));
+                    break;
+                }
+            }
         }
 
         try {
