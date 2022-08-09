84d16add802e77515090ed42d4b2f7e4f2aea77c
zhaokai <506683059@qq.com>
2022-08-09 17:09:08 +0800

    重写hashcode和equals方法

--- src/main/java/com/github/zk/heatmap/entity/HeatMapEntity.java
+++ src/main/java/com/github/zk/heatmap/entity/HeatMapEntity.java
@@ -1,5 +1,7 @@
 package com.github.zk.heatmap.entity;
 
+import java.util.Objects;
+
 /**
  * @author zk
  * @date 2019/8/2 9:29
@@ -31,5 +33,22 @@ public class HeatMapEntity {
 
     public void setValue(double value) {
         this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        HeatMapEntity that = (HeatMapEntity) o;
+        return Double.compare(that.lat, lat) == 0 && Double.compare(that.lon, lon) == 0 && Double.compare(that.value, value) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(lat, lon, value);
     }
 }
